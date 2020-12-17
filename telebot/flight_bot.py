1ac842cf3f5fa8a7a03bda90976c395b829b0950
Дмитрий Азаров <dmi-azarov@ya.ru>
2020-12-18 01:15:45 +0300

    ...

--- telebot/flight_bot.py
+++ telebot/flight_bot.py
@@ -11,7 +11,6 @@ from random import choice
 
 bot = telebot.TeleBot('1366677314:AAFTpl-zPAFTRCcjuqG2Xc1EOvAAPjmeeVo')
 
-# user_id = message.from_user.id - извлечение id пользователя
 # в пин закрепить слоган
 # TODO написать новые 4 команды в каждом новом хендлере
 
@@ -24,7 +23,6 @@ def welcome(message):
     bot.send_sticker(message.chat.id, sti)
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
-
     bot.send_message(message.chat.id,
                      'Привет, {0.first_name}!'  # имя пользователя и другие его учетные данные извлекаются только при помощи 0.first_name}. и format(message.from_user, bot.get_me())
                      '\nЯ робот, призванный отвечать на вопросы бортпроводников: '
@@ -32,14 +30,13 @@ def welcome(message):
                      'подсказать как настроить корпоративную почту, явка по форме или нет? и т.д.\n'
                      'Задавай свой первый вопрос.'
                      .format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
-    # keyboard
 
-    # ReplyKeyboardMarkup - не привязывается к сообщению # InlineKeyboardMarkup — Она привязывается к сообщению, с которым была отправлена.
-    btn1 = types.KeyboardButton('Перейти в OpenSky')
+    btn1 = types.KeyboardButton('OpenSky')
     btn2 = types.KeyboardButton('План работ')
     btn3 = types.KeyboardButton('Мой налет')
-    btn4 = types.KeyboardButton('Написать разработчику')
-    markup.add(btn1, btn2, btn3, btn4)
+    btn4 = types.KeyboardButton('Расчётный лист')
+    btn5 = types.KeyboardButton('Написать разработчику') # TODO перестала появляться клавиатура
+    markup.add(btn1, btn2, btn3, btn4, btn5)
 
 
 def find(question, user_request):
@@ -54,6 +51,15 @@ def find(question, user_request):
 @bot.message_handler(content_types=["text"])  # эта функция будет вызываться каждый раз, когда боту напишут текст
 def conversation(message):
     """Модуль для общения и взаимодействия с пользователем."""
+
+    def open():
+        """Предлагает открыть сайт"""
+        download_btn: InlineKeyboardMarkup = types.InlineKeyboardMarkup()
+        btn = types.InlineKeyboardButton(text="ОТКРЫТЬ", url=baza.dictionary[id]['link'])
+        download_btn.add(btn)
+        bot.send_message(message.chat.id, baza.dictionary[id]['answer'], reply_markup=download_btn)
+        bot.send_message(157758328, "Предложили ОТКРЫТЬ сайт или страницу по запросу: " + message.text)
+        found_result = True
 
     def download():
         """Предлагает скачать файл"""
@@ -93,7 +99,7 @@ def conversation(message):
                    types.InlineKeyboardButton("Всё верно", callback_data="Всё верно"))
         return markup
 
-    @bot.callback_query_handler(func=lambda call: True) # Хендлер для работы с существующими сообщениями????
+    @bot.callback_query_handler(func=lambda call: True)  # Хендлер для работы с существующими сообщениями????
     def callback_query(call):
         answer = ''
         if call.data == "Отредактировать ответ":
@@ -102,9 +108,8 @@ def conversation(message):
             bot.send_message(157758328, "Пользователь сообщил, что всё верно")
             answer = choice(baza.best_wishes)   # TODO вытащить id сообщения до кнопок
         bot.send_message(call.message.chat.id, answer)  # может только одну функцию вызывать # если взаимодействуем с инлайном и нужно отправить текстовое сообщение в ответ, то используем не chat.id, а call.message.chat.id, если хотим отправить короткое уведомление, то bot.answer_callback_query(call.id, "Answer is Yes")
-        bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id) # убирает клаиватуру после нажатия кнопок
+        bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)     # убирает клаиватуру после нажатия кнопок
         bot.delete_message(call.message.chat.id, call.message.message_id)
-
 
     def checking_answer(check_answer=None):  # выводит эти кнопки только еслив строгом соответсвии было выдано, потому что там return
         bot.send_message(message.chat.id, check_answer, reply_markup=correcting_button())
@@ -142,30 +147,38 @@ def conversation(message):
         bot.send_message(message.chat.id, 'Слишком короткий запрос. Пожалуйста, чуть подробнее.')
         return
 
-    if not found_result:
+    if not found_result:            # СТРОГОЕ СООТВЕТСТВИЕ
         for id in baza.dictionary:
             question = baza.dictionary[id]['question'].lower()
-            if message.text.lower() in question:                        # СТРОГОЕ СООТВЕТСТВИЕ
-                if 'скачать' in question:
+            if message.text.lower() in question:
+                if 'скачать' in question:   # так надо 2 раза
                     download()
-                else:
+                    found_result = True
+                elif 'просмотреть' in question:   # так надо 2 раза
+                    open()
+                    found_result = True
+                else:   # так надо 2 раза
                     bot.send_message(message.chat.id, baza.dictionary[id]['answer'])
                     bot.send_message(157758328,
-                                     "Информация выдана успешно в строгом соответствии по запросу: " + message.text)
+                                     "1 - Информация выдана успешно в строгом соответствии по запросу: " + message.text)
                     found_result = True  # вопрос checking_answer() для строго соответсвия вынесен в конец скрипта
 
-    if not found_result:
+    if not found_result:            # НЕ СТРОГОЕ СООТВЕТСВИЕ
         for id in baza.dictionary:
             question = baza.dictionary[id]['question'].lower()
-            if changed(message.text) in changed(question):              # НЕ СТРОГОЕ СООТВЕТСВИЕ
+            if changed(message.text) in changed(question):
                 if 'скачать' in question:
                     download()
+                    found_result = True
+                elif 'просмотреть' in question:
+                    open()
+                    found_result = True
                 else:
                     bot.send_message(message.chat.id, baza.dictionary[id]['answer'])
-                    bot.send_message(157758328, "какая-то информация выдана не в строгом соответсвии по запросу: " + message.text)
+                    bot.send_message(157758328, "2 - какая-то информация выдана не в строгом соответсвии по запросу: " + message.text)
                     found_result = True
 
-    if not found_result:                                                # ИЩЕТ В ЛЮБОМ ПОРЯДКЕ В РАМКАХ ВОПРОСА
+    if not found_result:            # ИЩЕТ В ЛЮБОМ ПОРЯДКЕ В РАМКАХ ВОПРОСА
         changed_user_request = changed(message.text).split()
         max_of_found_words = 0  # в max <- записывается matches <- записывается find(вычисляется количество совпадений слов)
         results = []
@@ -178,17 +191,17 @@ def conversation(message):
                 results.clear()                                 # очищаем список результатов
                 max_of_found_words = matches                    # в максимум записываем новую цифру соответсвия
                 results.append(baza.dictionary[id]['answer'])   # в результаты добавляем answer
-                                                                    # написать обратное условие, что если не будет найдено по вопросам то поискать по ответам
+
         if len(results) < 8:    # выдает ответы при оптимальном количстве результатов
             for each_answer in results:
                 bot.send_message(message.chat.id, each_answer)
-                bot.send_message(157758328, "выдана информация из слов в случайном порядке по запросу: " + message.text)
+                bot.send_message(157758328, "3 - выдана информация из слов в случайном порядке по запросу: " + message.text)
                 found_result = True
 
         if len(results) >= 8:   # не выдает ответы если их 8, крайне редко когда достигается, по другим методам поиска все равно сипит кучу ответов
             bot.send_message(message.chat.id, 'Найдено слишком много ответов. Пожалуйста, уточните свой вопрос или '
-                                          'спросите по-другому.')
-
+                          'спросите по-другому.')
+    # TODO написать обратное условие, что если не будет найдено по вопросам то поискать по ответам
     if not found_result:    # если ничего не найдено
         message.text = "Пользователь {0.first_name} {0.last_name} @{0.username} id{0.id} не смог найти запрос:\n" \
             .format(message.from_user, message.from_user, message.from_user, message.from_user) + message.text
@@ -205,8 +218,10 @@ def conversation(message):
     if found_result:  # две кнопки для списка ответов строгого соответствия
         checking_answer("Всё верно? Есть ошибки?")
 
-
-#   # bot.reply_to(message, message.text) - ответить переслав сообщение обратно
+# ReplyKeyboardMarkup - не привязывается к сообщению
+# InlineKeyboardMarkup — Она привязывается к сообщению, с которым была отправлена.
+# user_id = message.from_user.id - извлечение id пользователя
+# bot.reply_to(message, message.text) - ответить переслав сообщение обратно
 # print(message) # распечатывает всю информацию о написавшем человеке и историю сообщений в виде словаря
 # print(message.text) # message.text - введенное сообщение
 
