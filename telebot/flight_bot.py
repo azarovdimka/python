82a9b84a4680586bb3476518ea9080b38aa11f77
Дмитрий Азаров <dmi-azarov@ya.ru>
2020-12-30 12:02:36 +0300

    добавлена общая клавиатура

--- telebot/flight_bot.py
+++ telebot/flight_bot.py
@@ -15,28 +15,31 @@ bot = telebot.TeleBot('1366677314:AAFTpl-zPAFTRCcjuqG2Xc1EOvAAPjmeeVo')
 # TODO написать новые 4 команды в каждом новом хендлере
 
 
+def general_menu():
+    general_menu = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+    btn1 = types.KeyboardButton('OpenSky')
+    btn2 = types.KeyboardButton('План работ')
+    btn3 = types.KeyboardButton('Мой налет')
+    btn4 = types.KeyboardButton('Расчётный лист')
+    btn5 = types.KeyboardButton('Внести информацию')    # InlineKeyBoard (callback_data='Внести информацию')
+    general_menu.add(btn1, btn2, btn3, btn4, btn5)
+    return general_menu
+
+
 @bot.message_handler(commands=['start'])  # приветсвенный стикер и приветственный текст при вступлении в группу
 def welcome(message):
     """При первом подключении пользователя к боту - выводит приветсвенный стикер, приветсвенную речь. Также в этой
     функции обозначены кнопки, которые будут всегда отображаться под полем ввода запроса."""
     sti = open('static/AnimatedSticker.tgs', 'rb')
     bot.send_sticker(message.chat.id, sti)
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
     bot.send_message(message.chat.id,
                      'Привет, {0.first_name}!'  # имя пользователя и другие его учетные данные извлекаются только при помощи 0.first_name}. и format(message.from_user, bot.get_me())
                      '\nЯ робот, призванный отвечать на вопросы бортпроводников: '
-                     'подготовиться к МКК и КПК, часы работы и номера телефонов отделов и супрервайзеров, '
-                     'подсказать как настроить корпоративную почту, явка по форме или нет? и т.д.\n'
+                     'вопросы к МКК и КПК, часы работы и номера телефонов отделов и супрервайзеров, '
+                     'настройки почты, аббревиатуры, инструктажи... '
                      'Задавай свой первый вопрос.'
-                     .format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
-
-    btn1 = types.KeyboardButton('OpenSky')
-    btn2 = types.KeyboardButton('План работ')
-    btn3 = types.KeyboardButton('Мой налет')
-    btn4 = types.KeyboardButton('Расчётный лист')
-    btn5 = types.KeyboardButton('Написать разработчику')     # TODO перестала появляться клавиатура
-    markup.add(btn1, btn2, btn3, btn4, btn5)
+                     .format(message.from_user, bot.get_me()), reply_markup=general_menu())      #  parse_mode='html',
 
 
 def find(question, user_request):
@@ -54,12 +57,11 @@ def conversation(message):
 
     def open():
         """Предлагает открыть сайт"""
-        download_btn: InlineKeyboardMarkup = types.InlineKeyboardMarkup()
+        download_btn: InlineKeyboardMarkup = types.InlineKeyboardMarkup()   # что такое двоеточие и что оно дает??? reformat сам так сделал
         btn = types.InlineKeyboardButton(text="ОТКРЫТЬ", url=baza.dictionary[id]['link'])
         download_btn.add(btn)
         bot.send_message(message.chat.id, baza.dictionary[id]['answer'], reply_markup=download_btn)
         bot.send_message(157758328, "Предложили ОТКРЫТЬ сайт или страницу по запросу: " + message.text)
-        found_result = True
 
     def download():
         """Предлагает скачать файл"""
@@ -68,7 +70,6 @@ def conversation(message):
         download_btn.add(btn)
         bot.send_message(message.chat.id, baza.dictionary[id]['answer'], reply_markup=download_btn)
         bot.send_message(157758328, "Предложили скачать файл по запросу: " + message.text)
-        found_result = True
 
     def changed(text):
         """Видоизменяет текст поступающего запроса от пользователя и искомого текста в базе для успешного поиска:
@@ -88,30 +89,42 @@ def conversation(message):
     def find_garbage(message):
         """Ищет лишние слова-сорняки, которые вешают программу (как, кто, где) и меняет их на пустую строку"""
         for word in baza.garbage:                       # для каждого слова в кортеже
-            if word.lower() in message.lower():                         # если это каждое слово есть в запросе
+            if word.lower() in message.lower():         # если это каждое слово есть в запросе
                 return message.replace(word, '')        # нам нужно удалить часть строки
         return message
+
+    # def adding_information():
+    #     """Вносит информацию пользователя в бащу данных и перезаписывает файл с базой данных."""
+    #     bot.send_message(message.chat.id, 'Для начала добавьте вопрос. Начните свое сообщение со слова "внести вопрос:". Например:\n\n внести вопрос: номер телефона представителя в Москве')
 
     def correcting_button():                            # две кнопки прикрепляемые к выдаваемому ответу
         markup = types.InlineKeyboardMarkup()
         markup.row_width = 2
-        markup.add(types.InlineKeyboardButton("Отредактировать", callback_data="Отредактировать ответ"),
+        markup.add(types.InlineKeyboardButton("Исправить", callback_data="Исправить ответ"),
                    types.InlineKeyboardButton("Всё верно", callback_data="Всё верно"))
         return markup
 
     @bot.callback_query_handler(func=lambda call: True)  # Хендлер для работы с существующими сообщениями????
     def callback_query(call):
         answer = ''
-        if call.data == "Отредактировать ответ":
-            answer = 'В следующем сообщении еще раз коротко напишите свой вопрос и свой вариант ответа в произвольной форме, но начинаться Ваше сообщение должно со слова "правка", например:\n\n Правка: Кто желает знать где сидит фазан? Правильный ответ: охотник.\n\n Пожалуйста, не забывайте пояснять к какому вопросу правка. Присланное Вами сообщение пока не привязывается к ранее выданному ответу)'
+        if call.data == "Исправить ответ":
+            answer = 'В следующем сообщении еще раз коротко напишите свой вопрос и свой вариант ответа в произвольной ' \
+                     'форме, но начинаться Ваше сообщение должно со слова "правка", например:\n\n Правка: Кто желает ' \
+                     'знать где сидит фазан? Правильный ответ: охотник.\n\n Пожалуйста, не забывайте пояснять к какому ' \
+                     'вопросу правка. Присланное Вами сообщение пока не привязывается к ранее выданному ответу)'
         elif call.data == "Всё верно":
             bot.send_message(157758328, "Пользователь сообщил, что всё верно")
             answer = choice(baza.best_wishes)   # TODO вытащить id сообщения до кнопок
         bot.send_message(call.message.chat.id, answer)  # может только одну функцию вызывать # если взаимодействуем с инлайном и нужно отправить текстовое сообщение в ответ, то используем не chat.id, а call.message.chat.id, если хотим отправить короткое уведомление, то bot.answer_callback_query(call.id, "Answer is Yes")
         bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)     # убирает клаиватуру после нажатия кнопок
         bot.delete_message(call.message.chat.id, call.message.message_id)
+        # if call.data == "Внести информацию":
+        #     send = bot.send_message(message.chat.id, 'Введи город')
+        #     bot.register_next_step_handler(send, call)
+        #     # log(message)
+        #     # answer = "Следующее сообщение начните со слова: внести"
 
-    def checking_answer(check_answer=None):  # выводит эти кнопки только еслив строгом соответсвии было выдано, потому что там return
+    def checking_answer(check_answer=None):  # выводит эти кнопки только если в строгом соответсвии было выдано
         bot.send_message(message.chat.id, check_answer, reply_markup=correcting_button())
 
     found_result = False  # результат поиска - стоит значение по умолчанию, что ничего не найдено чтобы выводил сообщение что он не смог ничего найти и написать разработчику
@@ -121,30 +134,41 @@ def conversation(message):
 
     if message.chat.type == 'private':
         if message.text.lower() in baza.greetings:
-            bot.send_message(message.chat.id, 'Привет! Буду рад тебе помочь, задавай свой вопрос.')
+            bot.send_message(message.chat.id, 'Привет! Буду рад тебе помочь, задавай свой вопрос.', reply_markup=general_menu())
             return
         if "спасибо" in message.text.lower() or message.text.lower() in baza.good_bye:   # "спасибо in" помогает избежать кучи разных ответов если пишут "спасибо за информацию" и подобное...
             bot.send_message(message.chat.id, choice(baza.best_wishes))
             bot.send_message(157758328, "Пользователь id{0.id} @{0.username} {0.last_name} {0.first_name} поблагодарил." \
-                .format(message.from_user, message.from_user, message.from_user, message.from_user))
+                .format(message.from_user, message.from_user, message.from_user, message.from_user), reply_markup=general_menu())
             return
 
     if 'правка' in message.text.lower() or 'предложить информацию' in message.text.lower():
         correct = "Пользователь id{0.id} @{0.username} {0.last_name} {0.first_name} предлоджил правку:\n" \
             .format(message.from_user, message.from_user, message.from_user, message.from_user) + message.text[7:]
-        bot.send_message(message.chat.id, 'Ваша информация успешно отправлена. После ее рассмотрения будут внесены соответсвующие изменения. \n'
+        bot.send_message(message.chat.id, 'Ваша информация успешно отправлена. После ее рассмотрения будут внесены '
+                                          'соответсвующие изменения. \n'
                                           'Спасибо за Ваше участие в улучшении Телеграм-Бота!\n'
-                                          'В будущем Вы можете предлагать любую свою информацию для внесения в базу данных, просто начав свое сообщение со слов "предложение информации" или "правка".')
+                                          'В будущем Вы можете предлагать любую свою информацию для внесения в базу '
+                                          'данных, просто начав свое сообщение со слов "предложение информации" или '
+                                          '"правка".', reply_markup=general_menu())
         bot.send_message(157758328, correct)
         return
 
     if "отчёт пользователю" in message.text.lower():
-        bot.send_message(111, "Юрий, добавчоный номер бухгалтерии успешно изменен на 1017.")
+        bot.send_message(406430959, "Это шесть компетенций, добавил слово концепция и пункты туда. Наберите еще раз "
+                                    "шесть компетениций или концепция. это?", reply_markup=general_menu())
         bot.send_message(157758328, "Отчет пользователю отправлен успешно")
         found_result = True  # вопрос checking_answer() для строго соответсвия вынесен в конец скрипта
 
+    if "внести информацию" in message.text.lower():
+        # adding_information()
+        bot.send_message(message.chat.id,
+                         'Начните свое сообщение со слова "предложить информацию:". Например:\n\n предложить информацию: '
+                         'номер телефона представителя в Москве 8(495)123-45-67', reply_markup=general_menu())
+        return
+
     if len(changed(message.text)) <= 2:
-        bot.send_message(message.chat.id, 'Слишком короткий запрос. Пожалуйста, чуть подробнее, или измените запрос.')
+        bot.send_message(message.chat.id, 'Слишком короткий запрос. Пожалуйста, чуть подробнее, или измените запрос.', reply_markup=general_menu())
         return
 
     # if not found_result:
@@ -200,15 +224,15 @@ def conversation(message):
                 results.append(baza.dictionary[id]['answer'])   # в результаты добавляем answer
 
         if len(results) < 8:    # выдает ответы при оптимальном количстве результатов
-            for each_answer in results:
-                bot.send_message(message.chat.id, each_answer)
+            for each_answer in results:                                                     # TODO добавить исклюбчение если это место вашает программу
+                bot.send_message(message.chat.id, each_answer, reply_markup=general_menu()) # TODO быть может для разгрузки памяти выдавать ответы генератором?
                 bot.send_message(157758328, "3 - выдана информация из слов в случайном порядке по запросу: " + message.text)
                 found_result = True
 
         if len(results) >= 8:   # не выдает ответы если их 8, крайне редко когда достигается, по другим методам поиска все равно сипит кучу ответов
             bot.send_message(message.chat.id, 'Найдено слишком много ответов. Пожалуйста, уточните свой вопрос или '
-                          'спросите по-другому.')
-    # TODO написать обратное условие, что если не будет найдено по вопросам то поискать по ответам
+                          'спросите по-другому.', reply_markup=general_menu())
+                                                                                                # TODO написать обратное условие, что если не будет найдено по вопросам то поискать по ответам
     if not found_result:    # если ничего не найдено
         message.text = "Пользователь {0.first_name} {0.last_name} @{0.username} id{0.id} не смог найти запрос:\n" \
             .format(message.from_user, message.from_user, message.from_user, message.from_user) + message.text
@@ -220,7 +244,9 @@ def conversation(message):
                          'Если вы заметите ошибки, устаревшую информцию '
                          'или обнаружите факты некорректной работы бота - просьба написать об этом также  '
                          'разработчику @letchikazarov.\n\n'
-                         'Либо вы можете самостоятельно внести информацию в базу данных, начав свое сообщение со слов "предложить информацию:". Информация бдует внесена через некоторое время после предварительной модерации.')
+                         'Либо вы можете самостоятельно внести информацию в базу данных, начав свое сообщение со слов '
+                         '"предложить информацию:". Информация бдует внесена через некоторое время после '
+                         'предварительной модерации.', reply_markup=general_menu())
 
     if found_result:  # две кнопки для списка ответов строгого соответствия
         checking_answer("Всё верно? Есть ошибки?")
