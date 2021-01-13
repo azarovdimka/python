937cfe5cc304b6f5fc825b0d6f49bbcb41130a38
Дмитрий Азаров <dmi-azarov@ya.ru>
2021-01-13 04:23:53 +0300

    добавлены try except

--- telebot/flight_bot.py
+++ telebot/flight_bot.py
@@ -27,7 +27,7 @@ def general_menu():
     return general_menu
 
 
-@bot.message_handler(commands=['start'])  # приветсвенный стикер и приветственный текст при вступлении в группу
+@bot.message_handler(commands=['start'])
 def welcome(message):
     """При первом подключении пользователя к боту - выводит приветсвенный стикер, приветсвенную речь. Также в этой
     функции обозначены кнопки, которые будут всегда отображаться под полем ввода запроса."""
@@ -39,8 +39,8 @@ def welcome(message):
                      '\nЯ робот, призванный отвечать на вопросы бортпроводников: '
                      'вопросы к МКК и КПК, часы работы и номера телефонов отделов и супрервайзеров, '
                      'настройки почты, аббревиатуры, инструктажи... '
-                     'Задавай свой первый вопрос.'
-                     .format(message.from_user, bot.get_me()), reply_markup=general_menu())  # parse_mode='html',
+                     'Задавай свой первый вопрос. Лучше коротко.'
+                     .format(message.from_user, bot.get_me()), reply_markup=general_menu())
 
 
 def find(question, user_request):
@@ -52,24 +52,24 @@ def find(question, user_request):
     return count
 
 
-@bot.message_handler(content_types=["text"])  # эта функция будет вызываться каждый раз, когда боту напишут текст
+@bot.message_handler(content_types=["text"])  #
 def conversation(message):
-    """Модуль для общения и взаимодействия с пользователем."""
+    """Модуль для общения и взаимодействия с пользователем. Декоратор будет вызываться когда боту напишут текст."""
 
     def open():
         """Предлагает открыть сайт"""
         download_btn: InlineKeyboardMarkup = types.InlineKeyboardMarkup()  # что такое двоеточие и что оно дает??? reformat сам так сделал
         btn = types.InlineKeyboardButton(text="ОТКРЫТЬ", url=baza.dictionary[id]['link'])
         download_btn.add(btn)
-        bot.send_message(message.chat.id, baza.dictionary[id]['answer'], reply_markup=download_btn)
-        bot.send_message(157758328, "Предложили ОТКРЫТЬ сайт или страницу по запросу: " + message.text)
+        bot.send_message(message.chat.id, baza.dictionary[id]['answer'], reply_markup=download_btn, parse_mode='Markdown')
+        bot.send_message(157758328, "Предложили ОТКРЫТЬ сайт по запросу: " + message.text)
 
     def download():
         """Предлагает скачать файл"""
         download_btn: InlineKeyboardMarkup = types.InlineKeyboardMarkup()
         btn = types.InlineKeyboardButton(text="СКАЧАТЬ", url=baza.dictionary[id]['link'])
         download_btn.add(btn)
-        bot.send_message(message.chat.id, baza.dictionary[id]['answer'], reply_markup=download_btn)
+        bot.send_message(message.chat.id, baza.dictionary[id]['answer'], parse_mode='Markdown', reply_markup=download_btn)
         bot.send_message(157758328, "Предложили скачать файл по запросу: " + message.text)
 
     def changed(text):
@@ -79,10 +79,11 @@ def conversation(message):
         return ' '.join(lower_text_without_ends)
 
     def find_exception(message):
-        """все запросы от пользователя (принятые слова) сначала прогоняет через словарь исключений, если функция находит
-        его там, то заменяет его на такое же развернутое значение, которое следует использовать при дальнейшем поиске."""
+        """все запросы от пользователя сначала прогоняет через словарь исключений, если функция находит его там, то
+        заменяет его на такое же развернутое значение, которое следует использовать при дальнейшем поиске. ищет слова
+        для преобразования чтобы обойти минимально допустимое разрешение на длину слова"""
         for id in baza.exceptions:
-            if message == baza.exceptions[id]['word']:  # ищет слова для преобразования чтобы обойти минимально допустимое разрешение на длину слова
+            if message == baza.exceptions[id]['word']:
                 message = baza.exceptions[id]['changed_word']
                 return message
         return message
@@ -94,16 +95,67 @@ def conversation(message):
                 return message.replace(word, '')  # нам нужно удалить часть строки
         return message
 
+    def find_non_strict_accordance(message):
+        """Ищет не в строгом соответсвии."""
+        for id in baza.dictionary:
+            question = baza.dictionary[id]['question'].lower()
+            if changed(message.text) in changed(question):
+                if 'скачать' in question:  # так надо 2 раза
+                    download()
+                elif 'просмотреть' in question:  # так надо 2 раза
+                    open()
+                else:  # так надо 2 раза
+                    bot.send_message(message.chat.id, baza.dictionary[id]['answer'], reply_markup=general_menu(),
+                                     parse_mode='Markdown')
+                    bot.send_message(157758328, "2 - информация выдана не в строгом соответсвии по запросу: " + message.text)
+                found_result = True
+                return found_result
+
+    def find_in_random_order(message):
+        """Ищет в случайном соответсвии."""
+        changed_user_request = changed(message.text).split()
+        max_of_found_words = 0  # в max <- записывается matches <- записывается find(вычисляется количество совпадений слов)
+        results = []
+        for id in baza.dictionary:
+            question = baza.dictionary[id]['question'].lower()
+            matches = find(question, changed_user_request)  # для каждого id мы проверяем кол-во соотв-х слов
+            if matches == max_of_found_words and matches != 0:  # если количество соответсвий равно максимуму
+                results.append(baza.dictionary[id]['answer'])  # ответ заносим в результы
+            if matches > max_of_found_words:  # если соответсвий  больше счетчика максимума
+                results.clear()  # очищаем список результатов
+                max_of_found_words = matches  # в максимум записываем новую цифру соответсвия
+                results.append(baza.dictionary[id]['answer'])  # в результаты добавляем answer
+
+        if len(results) < 8:  # выдает ответы при оптимальном количстве результатов
+            for each_answer in results:
+                if 'скачать' in results:  # так надо 2 раза
+                    download()
+                    found_result = True
+                elif 'просмотреть' in results:  # так надо 2 раза
+                    open()
+                    found_result = True
+                else:  # так надо 2 раза
+                    bot.send_message(message.chat.id, each_answer, reply_markup=general_menu(),
+                                     parse_mode='Markdown')
+                    found_result = True
+                bot.send_message(157758328, "3 - выдана информация в случайном порядке по запросу: " + message.text)
+                return found_result
+
+        if len(results) >= 8:  # не выдает ответы если их 8, крайне редко когда достигается
+            bot.send_message(message.chat.id, 'Найдено слишком много ответов. Пожалуйста, уточните свой вопрос или '
+                                              'спросите по-другому.', reply_markup=general_menu(),
+                             parse_mode='Markdown')
+
     # def adding_information():
     #     """Вносит информацию пользователя в бащу данных и перезаписывает файл с базой данных."""
     #     bot.send_message(message.chat.id, 'Для начала добавьте вопрос. Начните свое сообщение со слова "внести вопрос:". Например:\n\n внести вопрос: номер телефона представителя в Москве')
 
-    def correcting_button():  # две кнопки прикрепляемые к выдаваемому ответу
-        markup = types.InlineKeyboardMarkup()
-        markup.row_width = 2
-        markup.add(types.InlineKeyboardButton("Исправить", callback_data="Исправить ответ"),
-                   types.InlineKeyboardButton("Всё верно", callback_data="Всё верно"))
-        return markup
+    # def correcting_button():  # две кнопки прикрепляемые к выдаваемому ответу
+    #     markup = types.InlineKeyboardMarkup()
+    #     markup.row_width = 2
+    #     markup.add(types.InlineKeyboardButton("Исправить", callback_data="Исправить ответ"),
+    #                types.InlineKeyboardButton("Всё верно", callback_data="Всё верно"))
+    #     return markup
 
     # @bot.callback_query_handler(func=lambda call: True)  # Хендлер для работы с существующими сообщениями????
     # def callback_query(call):
@@ -128,7 +180,7 @@ def conversation(message):
     # def checking_answer(check_answer=None):  # выводит эти кнопки только если в строгом соответсвии было выдано
     #     bot.send_message(message.chat.id, check_answer, reply_markup=correcting_button())
 
-    found_result = False  # результат поиска - стоит значение по умолчанию, что ничего не найдено чтобы выводил сообщение что он не смог ничего найти и написать разработчику
+    found_result = False
 
     message.text = find_exception(message.text.lower())
     message.text = find_garbage(message.text)
@@ -138,7 +190,7 @@ def conversation(message):
             bot.send_message(message.chat.id, 'Привет! Буду рад тебе помочь, задавай свой вопрос.',
                              reply_markup=general_menu())
             return
-        if "спасибо" in message.text.lower() or message.text.lower() in baza.good_bye:  # "спасибо in" помогает избежать кучи разных ответов если пишут "спасибо за информацию" и подобное...
+        if "спасибо" in message.text.lower() or message.text.lower() in baza.good_bye:
             bot.send_message(message.chat.id, choice(baza.best_wishes))
             bot.send_message(157758328, "Пользователь id{0.id} @{0.username} {0.last_name} {0.first_name} поблагодарил." \
                              .format(message.from_user, message.from_user, message.from_user, message.from_user),
@@ -188,85 +240,58 @@ def conversation(message):
         bot.send_message(message.chat.id, 'Слишком короткий запрос. Пожалуйста, чуть подробнее, или измените запрос.',
                          reply_markup=general_menu())
         return
-
     # if not found_result:
     #     words = changed(message.text).split(' ') # TODO исли использовать этот способ фильтрации то нельзя будет предлоги использовать в запросе и преобразовывать выражения в слова с предогами
     #     for letter in words:
     #         if len(letter) <= 1:
     #             bot.send_message(message.chat.id, 'Слишком короткий запрос. Пожалуйста, чуть подробнее.')
     #             return
-
-    if not found_result:  # СТРОГОЕ СООТВЕТСТВИЕ
+    if not found_result:  # СТРОГОЕ СООТВЕТСТВИЕ        # if можно заменить на while и засунуть всё в один try except
         for id in baza.dictionary:
             question = baza.dictionary[id]['question'].lower()
             if message.text.lower() in question:
                 if 'скачать' in question:  # так надо 2 раза
-                    download()
+                    download()  # TODO кнопки скачать и просмотреть не передаются через try except
                     found_result = True
                 elif 'просмотреть' in question:  # так надо 2 раза
                     open()
                     found_result = True
                 else:  # так надо 2 раза
-                    bot.send_message(message.chat.id, baza.dictionary[id]['answer'], reply_markup=general_menu(),
+                    try:
+                        bot.send_message(message.chat.id, baza.dictionary[id]['answer'], reply_markup=general_menu(), # TODO если вешается бот то ругается на ключ answer в 260 строке
                                      parse_mode='Markdown')
-                    bot.send_message(157758328,
-                                     "1 - Информация выдана успешно в строгом соответствии по запросу: " + message.text)
-                    found_result = True  # вопрос checking_answer() для строго соответсвия вынесен в конец скрипта
+                        bot.send_message(157758328,
+                                     "1 - Информация выдана в строгом соответствии по запросу: " + message.text)
+                    except Exception as exc:
+                        bot.send_message(157758328,
+                                         f"при запросе '{message.text}' при поиске в строгом соответствии возникала ошибка {type(exc).__name__} {exc} ")
+                    found_result = True
+
 
     if not found_result:  # НЕ СТРОГОЕ СООТВЕТСВИЕ
-        for id in baza.dictionary:
-            question = baza.dictionary[id]['question'].lower()
-            if changed(message.text) in changed(question):
-                if 'скачать' in question:
-                    download()
-                    found_result = True
-                elif 'просмотреть' in question:
-                    open()
-                    found_result = True
-                else:
-                    bot.send_message(message.chat.id, baza.dictionary[id]['answer'], reply_markup=general_menu(),
-                                     parse_mode='Markdown')
-                    bot.send_message(157758328,
-                                     "2 - какая-то информация выдана не в строгом соответсвии по запросу: " + message.text)
-                    found_result = True
+        try:
+            found_result = find_non_strict_accordance(message)
+        except Exception as exc:
+            bot.send_message(157758328, f"при запросе '{message.text}' в нестрогом соответсвии возникала ошибка {type(exc).__name__} {exc}")
 
     if not found_result:  # ИЩЕТ В ЛЮБОМ ПОРЯДКЕ В РАМКАХ ВОПРОСА
-        changed_user_request = changed(message.text).split()
-        max_of_found_words = 0  # в max <- записывается matches <- записывается find(вычисляется количество совпадений слов)
-        results = []
-        for id in baza.dictionary:
-            question = baza.dictionary[id]['question'].lower()
-            matches = find(question, changed_user_request)  # для каждого id мы проверяем кол-во соотв-х слов
-            if matches == max_of_found_words and matches != 0:  # если количество соответсвий равно максимуму
-                results.append(baza.dictionary[id]['answer'])  # ответ заносим в результы
-            if matches > max_of_found_words:  # если соответсвий  больше счетчика максимума
-                results.clear()  # очищаем список результатов
-                max_of_found_words = matches  # в максимум записываем новую цифру соответсвия
-                results.append(baza.dictionary[id]['answer'])  # в результаты добавляем answer
-
-        if len(results) < 8:  # выдает ответы при оптимальном количстве результатов
-            for each_answer in results:  # TODO добавить исклюбчение если это место вашает программу
-                bot.send_message(message.chat.id, each_answer, reply_markup=general_menu(),
-                                 parse_mode='Markdown')  # TODO быть может для разгрузки памяти выдавать ответы генератором?
-                bot.send_message(157758328,
-                                 "3 - выдана информация из слов в случайном порядке по запросу: " + message.text)
-                found_result = True
-
-        if len(
-                results) >= 8:  # не выдает ответы если их 8, крайне редко когда достигается, по другим методам поиска все равно сипит кучу ответов
-            bot.send_message(message.chat.id, 'Найдено слишком много ответов. Пожалуйста, уточните свой вопрос или '
-                                              'спросите по-другому.', reply_markup=general_menu(),
+        try:
+            found_result = find_in_random_order(message)
+        except Exception as exc:
+            # TODO придумать как изменить запрос (персетавить местами слова??) чтобы обойти ошибку
+            bot.send_message(message.chat.id,
+                             'Не удалось найти ответ. Попробуйте упростить свой запрос.',
+                             reply_markup=general_menu(),
                              parse_mode='Markdown')
-            # TODO написать обратное условие, что если не будет найдено по вопросам то поискать по ответам
+            bot.send_message(157758328, f"при запросе '{message.text}' при поиске в случайном порядке возникала ошибка {type(exc).__name__} {exc} ")
+
     if not found_result:  # если ничего не найдено
         message.text = "Пользователь {0.first_name} {0.last_name} @{0.username} id{0.id} не смог найти запрос:\n" \
                            .format(message.from_user, message.from_user, message.from_user,
                                    message.from_user) + message.text
-
-        bot.send_message(157758328,
-                         message.text)  # если запрос ненайден - бот об этом сообщит разрабочтику дублированием сообщения напрямую
+        bot.send_message(157758328, message.text)
         bot.send_message(message.chat.id,
-                         '{0.first_name}, я не знаю, что на это ответить. Попробуйте изменить свой запрос.\n'
+                         '{0.first_name}, я не знаю, что на это ответить. Попробуйте изменить свой запрос.\n\n'
                          'Если у вас появится какая-то информация на этот вопрос - сообщите, пожалуйста.\n'
                          'Ваш неудачный запрос уже направлен разработчику на рассмотрение.\n'
                          '\n Если вы заметите ошибки, устаревшую информцию '
@@ -274,7 +299,6 @@ def conversation(message):
                          'разработчику @letchikazarov.\n'
                          'Либо вы можете самостоятельно внести информацию в базу данных, нажав кнопку "Добавить информацию".'.format(
                              message.from_user, bot.get_me()), reply_markup=general_menu())
-
     # if found_result:  # две кнопки для списка ответов строгого соответствия
     #     checking_answer("Всё верно? Есть ошибки?")
 
@@ -285,5 +309,4 @@ def conversation(message):
 # bot.reply_to(message, message.text) - ответить переслав сообщение обратно
 # print(message) # распечатывает всю информацию о написавшем человеке и историю сообщений в виде словаря
 # print(message.text) # message.text - введенное сообщение
-
 bot.polling(none_stop=True)  # запускает бота
