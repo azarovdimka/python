e303803bcf79f3f789a4847688c79e7543b9ccd1
Дмитрий Азаров <dmi-azarov@ya.ru>
2021-09-03 01:17:40 +0300

    проверяет новые документы в opensky

---
+++ telebot/check_news.py
@@ -0,0 +1,49 @@
+import requests
+import dict_users
+from bs4 import BeautifulSoup
+
+
+def parser(user_id):  # это надо было все обернуть в функцию чтобы потом при импорте вызвать модуль.функция()
+    url = 'https://edu.rossiya-airlines.com/'
+
+    s = requests.Session()
+
+    data = {
+        'refer': 'https://edu.rossiya-airlines.com//',
+        'login': '1',
+        'user_id': '',
+        'backend_url': 'https://sup.rossiya-airlines.com:8080',
+        'username': '119221',  # dict_users.users[user_id]['tab_number'],  # '119229',
+        'userpass': '2DH64rf2',  # dict_users.users[user_id]['password'],  # 'Parshina15',
+        'domain': 'stc.local',
+        'submit': 'войти'
+    }
+
+    main_page = s.post(url, data=data, headers=dict(Referer=url))  # main_page = response 200
+    soup = BeautifulSoup(main_page.content, 'html.parser')  # .find_all('div', {'class': ['dhx_cal_event_line_start']})
+
+    events = soup.select('.table.table-striped.table-condensed.table-hover')
+
+    doc_list = []
+    report = ''
+
+    try:
+        table = events[0]
+    except Exception:
+        return
+    tbody = table.contents[1]
+    rows = tbody.contents
+
+    for tr in rows:
+        cells = tr.contents
+        name_document = cells[1].text
+        date_exp = cells[2].text
+        name_button = cells[3].text  # Скачать или Подтвердить
+
+        if name_button == "Скачать":
+            doc_list.append(name_document)
+    if len(doc_list) != 0:
+        report += f"Появились новые документы в опенскае: {doc_list}"
+        return report
+    else:
+        return None
