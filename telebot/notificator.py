58e017294443df13a9e753d7799c0b1b44ac5fd0
Дмитрий Азаров <dmi-azarov@ya.ru>
2021-10-05 22:39:19 +0300

    правки

--- telebot/notificator.py
+++ telebot/notificator.py
@@ -10,39 +10,32 @@ import os
 
 
 def write_check_relevance(plan, chat_id):  # TODO НЕ ЗАБУДЬ ПОМЕНЯТЬ АДРЕС!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    file_path = "C:\\PycharmProjects\\Probe\\мои примеры\\GitHub\\telebot\\plans\\plans" + str(
-        chat_id) + ".txt"  # "/usr/local/bin/bot/plans/plans" + str(chat_id) + ".txt" #
-    #
+    file_path = "/usr/local/bin/bot/plans/plans" + str(
+        chat_id) + ".txt"  # "C:\\PycharmProjects\\Probe\\мои примеры\\GitHub\\telebot\\plans\\plans" + str(chat_id) + ".txt"  # "/usr/local/bin/bot/plans/plans" + str(chat_id) + ".txt" #
 
-    # если пароля у пользователя в словаре нет - то для него он не будет дальше проверять и функция закончится
     if dict_users.users[chat_id]['password'] == '' or not dict_users.users[chat_id]['plan_notify']:
         return None
-    # если файла с планом ранее не существоало
     if not os.path.exists(file_path):
-        # создаст файл и запищет в него план полученный через параметры без дополнительных сравнений и проверок
         with open(file_path, 'w', encoding='utf-8') as modified:
             modified.write(plan)
-            return plan  # отправлять уведомление с новыми рейсами
-    # если пароль в словаре есть и какой-то файл уже был
+            return plan
     else:
-        # читает старый файл
         with open(file_path, 'r', encoding='utf-8') as original:
             old_file = original.read()
-
         with open(file_path, encoding='utf-8') as original:
-            lines_old_file = original.readlines()  # преобразовали в список для будущего построчного сравнения
-        # если имеющийся файл совпадает с полученными данными парсера
+            lines_old_file = original.readlines()
         if plan == old_file:
-            return  # ничего не происходит, ничего не уведомляет # TODO быть может так что файлы равны, и программа не завершается, а интерпретатор продолжает читать код дальше и
-        file_lines = []  # создание нового списка строк из файла без \n для корректного сравнения строк
+            return
+        file_lines = []
         for i in lines_old_file:
             file_lines.append(i[:-1])  # убираем символ переноса каретки
-
         plan_list = plan.split('\n')  # создает из плана список чтобы можно было построчно сравнивать
-        # если первая строка не совпадает и длина полученного плана меньше на одну строку:
+        if len(plan_list) > 1:
+            del plan_list[-1]
         try:
-            if file_lines[1] != plan_list[1] and (len(file_lines) - len(
-                    plan_list) + 1 == 1):  # приходится плюсовать единицу, потому что при plan.split('\n') он бобавляет в конце еще одну пустую строку, так как в конце последней строки тоже стоит \n
+            if file_lines[1] != plan_list[1] and (len(file_lines) - len(plan_list) == 1) and file_lines[
+                                                                                             2:] == plan_list[
+                                                                                                    1:]:  # приходится плюсовать единицу, потому что при plan.split('\n') он бобавляет в конце еще одну пустую строку, так как в конце последней строки тоже стоит \n
                 with open(file_path, 'w', encoding='utf-8') as modified:
                     modified.write(plan)  # изменения в файл запишет
                     return  # но уведомлять не будет если ночью просто прошел день, а соовтетсенно рейс отлетал, а нового ничего не появилось
