1eb7176a47bb20956895f9a08cf200154745a32d
Дмитрий Азаров <dmi-azarov@ya.ru>
2021-11-27 02:09:07 +0300

    правки

--- telebot/exception_logger.py
+++ telebot/exception_logger.py
@@ -2,21 +2,22 @@ import traceback
 import time
 
 
-def writer(exc, request, user_id, fio=None,
-           answer=None):  # TODO НЕ ЗАБУДЬ ПОМЕНЯТЬ АДРЕС!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    file_path = "C:\\PycharmProjects\\Probe\\мои примеры\\GitHub\\telebot\\exception_log\\exception_log.txt"  # "/usr/local/bin/bot/exception_log/exception_log.txt" #
+def writer(exc, request, user_id, fio=None, answer=None):  # TODO НЕ ЗАБУДЬ ПОМЕНЯТЬ АДРЕС!!!!!!!!!!!
+    file_path = "/usr/local/bin/bot/exception_log/exception_log.txt"  # "C:\\PycharmProjects\\Probe\\мои примеры\\GitHub\\telebot\\exception_log\\exception_log.txt"  #
     datetime = time.strftime('%d.%m.%Y %H:%M')
     text_error = f"Пользователь: {user_id} {fio}\n" \
                  f"При запросе: {request}\n" \
                  f"Ответ пользователю: {answer}\n" \
                  f"Возникала ошибка: {type(exc).__name__} {exc} \n" \
                  f"Полный traceback:{traceback.format_exc()}\n\n"
-    with open(file_path, 'r', encoding='utf-8') as writed_file:
-        if text_error in writed_file:
+    with open(file_path, 'r', encoding='utf-8') as old_file:
+        old_file = old_file.read()
+        if text_error in old_file:
             return
         else:
-            with open(file_path, 'a', encoding='utf-8') as original:
-                original.write(f"\n\n{datetime}\n" \
+            with open(file_path, 'a', encoding='utf-8') as new_data:
+                new_data.write(f"\n\n{datetime}\n" \
                                f"{text_error}")
+                return "!!! Внесена запись о возникшей ошибке в лог ошибок"
 
 # TODO НЕ ЗАБУДЬ ПОМЕНЯТЬ АДРЕС!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
