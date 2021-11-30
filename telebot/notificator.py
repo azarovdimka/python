c30167542b0918b18d79ab5886e643619fde29ae
Дмитрий Азаров <dmi-azarov@ya.ru>
2021-11-30 22:02:57 +0300

    правки

--- telebot/notificator.py
+++ telebot/notificator.py
@@ -11,47 +11,47 @@ import os
 
 def write_check_relevance(plan, chat_id):  # TODO НЕ ЗАБУДЬ ПОМЕНЯТЬ АДРЕС!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     file_path = "/usr/local/bin/bot/plans/plans" + str(
-        chat_id) + ".txt"  # "C:\\PycharmProjects\\Probe\\мои примеры\\GitHub\\telebot\\plans\\plans" + str(chat_id) + ".txt"  # "/usr/local/bin/bot/plans/plans" + str(chat_id) + ".txt" #
+        chat_id) + ".txt"  # "C:\\PycharmProjects\\Probe\\мои примеры\\GitHub\\telebot\\plans\\plans" + str(chat_id) + ".txt"  #  "/usr/local/bin/bot/plans/plans" + str(chat_id) + ".txt" #
 
     if dict_users.users[chat_id]['password'] == '' or not dict_users.users[chat_id]['plan_notify']:
         return None
+
     if not os.path.exists(file_path):
         with open(file_path, 'w', encoding='utf-8') as modified:
             modified.write(plan)
             return plan
+
     else:
         with open(file_path, 'r', encoding='utf-8') as original:
             old_file = original.read()
         with open(file_path, encoding='utf-8') as original:
             lines_old_file = original.readlines()
+
         if plan == old_file:
             return
+
         file_lines = []
+
         for i in lines_old_file:
             file_lines.append(i[:-1])  # убираем символ переноса каретки
+
         plan_list = plan.split('\n')  # создает из плана список чтобы можно было построчно сравнивать
         if len(plan_list) > 1:
             del plan_list[-1]
-        try:
-            if file_lines[1] != plan_list[1] and (len(file_lines) - len(plan_list) == 1) and file_lines[
-                                                                                             2:] == plan_list[
-                                                                                                    1:]:  # приходится плюсовать единицу, потому что при plan.split('\n') он бобавляет в конце еще одну пустую строку, так как в конце последней строки тоже стоит \n
-                with open(file_path, 'w', encoding='utf-8') as modified:
-                    modified.write(plan)  # изменения в файл запишет
-                    return  # но уведомлять не будет если ночью просто прошел день, а соовтетсенно рейс отлетал, а нового ничего не появилось
-        except Exception as exc:  # todo попробовать перенести except в 42 строку чтобы witn open два раза не вызывать
-            exception_logger.writer(exc=exc, request='сравнение старого и нового плана на предмет прошедшего рейса',
-                                    user_id=dict_users.users[chat_id])
-            with open(file_path, 'w', encoding='utf-8') as modified:
-                modified.write(plan)  # изменения в файл запишет
+            try:
+                if file_lines[1] != plan_list[1] and (len(file_lines) - len(plan_list) == 1):
+                    with open(file_path, 'w', encoding='utf-8') as modified:
+                        modified.write(plan)  # изменения в файл запишет
+                        return  # но уведомлять не будет если ночью просто прошел день, а соовтетсенно рейс отлетал, а нового ничего не появилось
+                else:
+                    with open(file_path, 'w', encoding='utf-8') as modified:
+                        modified.write(plan)  # изменения в файл запишет
+                        return plan  # уведомит новым планом
 
+            except Exception as exc:  # todo попробовать перенести except в 42 строку чтобы witn open два раза не вызывать
+                exception_logger.writer(exc=exc, request='сравнение старого и нового плана на предмет прошедшего рейса',
+                                        user_id=dict_users.users[chat_id])
                 return
-        # если файлы не совпадают
-        else:
-            # перезаписывает файл полностью на новые данные
-            with open(file_path, 'w', encoding='utf-8') as modified:
-                modified.write(plan)
-                return plan  # уведомит новым планом
 
 
 # TODO НЕ ЗАБУДЬ ПОМЕНЯТЬ АДРЕС!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -64,7 +64,6 @@ def notify(user_id):
         result = write_check_relevance(plan,
                                        user_id)  # TODO если эта функция возвращает None, то функция просто должна завершиться, но plan=None может послужить причиной исключения
         if result:
-            # print(result)
             return result
         else:
             return
