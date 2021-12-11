75bb8ce90b6caaa2fe609f84dae16ce52684c83e
Дмитрий Азаров <dmi-azarov@ya.ru>
2021-12-11 21:46:41 +0300

    правки

--- telebot/flight_counter.py
+++ telebot/flight_counter.py
@@ -31,10 +31,10 @@ def parser(user_id, tab_number,
 
     s.post(url, data=data, headers=dict(Referer=url))  # work_plan = response 200
 
-    if dict_users.users[user_id]['password'] == '':
-        return f"{dict_users.users[user_id]['name']} мы не можем подсчитать за вас рейсы, так как нам неимзвестен ваш пароль от OpenSky"
+    if password == '' or password == '0' or password is False:
+        return "Посчитать рейсы невозможно, так как неизвестен ваш пароль от OpenSky."
 
-    if month == '12':
+    if month == '01':
         year = str(int(time.strftime('%Y')) - 1)
 
     url = 'https://edu.rossiya-airlines.com/workplan/'
@@ -53,33 +53,28 @@ def parser(user_id, tab_number,
     try:
         table = events[0]
     except Exception:
-        error = f"Проблема с подсчетом количества рейсов. Вероятно, в базе указан неверный логин {dict_users.users[user_id]['tab_number']} и пароль {dict_users.users[user_id]['password']}"
+        error = f"Проблема с подсчетом количества рейсов. Вероятно, в базе указан неверный пароль {password}"
         return error
     tbody = table.contents[1]
     rows = tbody.contents
 
-    flight_list_prev = []
     general_types_counter_till_18_prev = 0
     sukhoj_till_18_prev = 0
-    counter_sukhoj_prev = 0
+    counter_shoulders_sukhoj_prev = 0
     general_counter_prev = 0
 
-    flight_list = []
     general_types_counter_till_18 = 0
     sukhoj_till_18 = 0
-    counter_sukhoj = 0
+    counter_shoulders_sukhoj = 0
     general_counter = 0
 
     for tr in rows:
 
         cells = tr.contents
-        # print(str(tr)[257:])
-        # print('')
-        day_month_year_start = cells[1].text.strip()  # 03.09.2021
-        # hm_msk_start = cells[2].text
+        day_month_year_start = cells[1].text.strip()
         departure_dt = f'{day_month_year_start}'  # + {hm_msk_start}
         dt_utc_arrive = datetime.strptime(departure_dt, '%d.%m.%Y').replace(tzinfo=pytz.utc)
-        dt_msk = dt_utc_arrive.astimezone(pytz.utc) + timedelta(days=1)
+        dt_msk = dt_utc_arrive.astimezone(pytz.utc) + timedelta(hours=3)
         month_site = int(dt_msk.month)
 
         if current_month < month_site:
@@ -91,63 +86,91 @@ def parser(user_id, tab_number,
 
         if current_month > month_site:  # для парсинга предыдущего месяца
 
-            if general_counter_prev <= 18:
-                general_types_counter_till_18_prev += 1
-                sukhoj_till_18_prev = counter_sukhoj_prev  # TODO эту строку удалить и счетчики тоже?
             if '/' in flight_number:
-                pair_flights_list = flight_number.split('/')
-                for flight in pair_flights_list:
-                    flight_list_prev.append(flight)  # TODO почему не написать проще pair_flight_list.count()?
-                    general_counter_prev += 1
-                if 'СУ' in aircraft:
-                    counter_sukhoj_prev += flight_number.count('/') + 1  # TODO уточнить систему расчетов
+                flights_list = flight_number.split('/')
+                for flight in flights_list:
+                    if 'п' in flight:
+                        flights_list.remove(flight)
+                general_counter_prev += len(flights_list)
+                if 'СУ' in aircraft or 'С9Н' in aircraft:
+                    counter_shoulders_sukhoj_prev += flight_number.count('/') + 1  # TODO уточнить систему расчетов
+                # if general_counter_prev <= 18 and '/' in flight_number:
+                # sukhoj_till_18_prev = general_counter_prev  # TODO эту строку удалить и счетчики тоже?
 
         if current_month == month_site:  # для парсинга текущего месяца
             last_date = cells[6].text[-21:-16]
-            if general_counter <= 18:
-                general_types_counter_till_18 += 1
-                sukhoj_till_18 = counter_sukhoj
             if '/' in flight_number:
-                pair_flights_list = flight_number.split('/')
-                for flight in pair_flights_list:
-                    if flight == '':
-                        continue
-                    flight_list.append(flight)
-                    general_counter += 1
-                if 'п' in flight_number:
-                    number_p = flight_number.count('п')
-                    general_counter -= number_p
-                if 'СУ' in aircraft:
-                    counter_sukhoj += flight_number.count('/') + 1  # TODO уточнить систему расчетов
+                flights_list = flight_number.split('/')
+                for flight in flights_list:
+                    if 'п' in flight:
+                        flights_list.remove(flight)
+                general_counter += len(flights_list)
+                if 'СУ' in aircraft or 'С9Н' in aircraft:
+                    counter_shoulders_sukhoj += flight_number.count('/') + 1  # TODO уточнить систему расчетов
+                # if general_counter <= 18:
+                # sukhoj_till_18 = general_counter
 
-    flight_counter_prev = len(flight_list_prev)
-    flight_counter = len(flight_list)
+    shoulder_counter_prev = general_counter_prev
+    shoulder_counter = general_counter
 
-    other_types_prev = flight_counter_prev - counter_sukhoj_prev
-    other_types = flight_counter - counter_sukhoj
+    if shoulder_counter_prev > counter_shoulders_sukhoj_prev:
+        other_types_shoulders_prev = shoulder_counter_prev - counter_shoulders_sukhoj_prev
+    else:
+        other_types_shoulders_prev = 0
+    if shoulder_counter > counter_shoulders_sukhoj:
+        other_types_shoulders = shoulder_counter - counter_shoulders_sukhoj
+    else:
+        other_types_shoulders = 0
 
     def get_end(number):
         if str(number)[-1] == '1' and number != 11:
             return ''
-        if 5 <= int(str(number)[-1]) <= 9 or int(str(number)[-1]) == 0:
+        if 5 <= int(str(number)[-1]) <= 9 or int(str(number)[-1]) == 0 or number == 11 or number > 11:
             return 'ов'
         else:
             return 'а'
 
-    output_info = f'За прошлый месяц у вас всего {flight_counter_prev} рейс{get_end(flight_counter_prev)}:\n ' \
-                  f'- {counter_sukhoj_prev} рейс{get_end(counter_sukhoj_prev)} на Сухом;\n ' \
-                  f'- {other_types_prev} рейс{get_end(other_types_prev)} на других типах.\n' \
-                  f'C 1 по 18 плечо выполнено {sukhoj_till_18_prev} на Cухом (+440 р/час)\n' \
-                  '\n' \
-                  f'За этот месяц c 1 по {last_date} у вас всего {flight_counter} рейс{get_end(flight_counter)}:\n ' \
-                  f'- {counter_sukhoj} рейс{get_end(counter_sukhoj)} на Сухом;\n ' \
-                  f'- {other_types} рейс{get_end(other_types)} на других типах.\n' \
-                  f'C 1 по 18 плечо выполнено {sukhoj_till_18} на Cухом (+440 р/час)\n' \
+    if shoulder_counter_prev % 2 == 0:
+        general_counter_prev = shoulder_counter_prev // 2
+    else:
+        general_counter_prev = shoulder_counter_prev / 2
+    if shoulder_counter % 2 == 0:
+        general_counter = shoulder_counter // 2
+    else:
+        general_counter = shoulder_counter / 2
+
+    if counter_shoulders_sukhoj_prev % 2 == 0:
+        flight_counter_sukhoi_prev = counter_shoulders_sukhoj_prev // 2
+    else:
+        flight_counter_sukhoi_prev = counter_shoulders_sukhoj_prev / 2
+    if counter_shoulders_sukhoj % 2 == 0:
+        flight_counter_sukhoi = counter_shoulders_sukhoj // 2
+    else:
+        flight_counter_sukhoi = counter_shoulders_sukhoj / 2
+
+    if other_types_shoulders_prev % 2 == 0:
+        other_types_prev = other_types_shoulders_prev // 2
+    else:
+        other_types_prev = other_types_shoulders_prev / 2
+
+    if other_types_shoulders % 2 == 0:
+        other_types = other_types_shoulders // 2
+    else:
+        other_types = other_types_shoulders / 2
+
+    if last_date == '':
+        last_date = current_date
+
+    output_info = f'За прошлый месяц у вас всего {general_counter_prev} рейс{get_end(general_counter_prev)}:\n ' \
+                  f'- {flight_counter_sukhoi_prev} рейс{get_end(flight_counter_sukhoi_prev)} на Сухом;\n ' \
+                  f'- {other_types_prev} рейс{get_end(other_types_prev)} на других типах.\n\n' \
+                  f'За этот месяц c 1 по {last_date} у вас всего {general_counter} рейс{get_end(general_counter)}:\n ' \
+                  f'- {flight_counter_sukhoi} рейс{get_end(flight_counter_sukhoi)} на Сухом;\n ' \
+                  f'- {other_types} рейс{get_end(other_types)} на других типах.\n\n' \
                   f'Если у Вас есть закрутки, переходящие на следующий месяц - пока такие случаи расчленять и считать не умею.'
 
     # print(output_info)
     return output_info
     # return "<pre>" + output_info + "</pre>"
-# TODO ПРОВЕРЬ ПРИНТЫ ЛОГИН И ПАРОЛЬ!!!!!!!!!!!!!!!!!!!!!!!!!
 
-# parser(157758328)
+# parser(816830262, '119182', 'Airbus339!')
