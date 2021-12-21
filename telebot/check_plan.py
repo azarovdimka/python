0264fcec9e03a82c0d31d80d08643d3054ee684e
Дмитрий Азаров <dmi-azarov@ya.ru>
2021-12-21 18:31:41 +0300

    правки

--- telebot/check_plan.py
+++ telebot/check_plan.py
@@ -4,6 +4,8 @@ import telebot
 from telebot.types import InlineKeyboardMarkup
 from telebot import types
 import time
+import os
+import check_news
 import settings
 import handler_db
 import notificator
@@ -11,13 +13,22 @@ import exception_logger
 
 bot = telebot.TeleBot(settings.TOKEN)
 
+document_btn: InlineKeyboardMarkup = types.InlineKeyboardMarkup()
+btn_doc = types.InlineKeyboardButton(text="Открыть подробнее в OpenSky", url='https://edu.rossiya-airlines.com/')
+document_btn.add(btn_doc)
+
+plan_btn: InlineKeyboardMarkup = types.InlineKeyboardMarkup()
+btn = types.InlineKeyboardButton(text="Открыть план работ в OpenSky", url='https://edu.rossiya-airlines.com/workplan/')
+plan_btn.add(btn)
+
 
 def cycle_plan_notify():
     while True:
         # bot.send_message(157758328, f'Бот начал проверку планов пользователей')
-        counter_users = 0
         sent_plan_counter = 0
+        sent_doc_counter = 0
         sent_plan_list = []
+        sent_doc_list = []
         current_time = time.strftime('%H:%M')
         if current_time == '00:00':
             time.sleep(60)
@@ -28,7 +39,6 @@ def cycle_plan_notify():
                 user_id, surname, name, tab_number, password, messaging, check_permissions, night_notify, plan_notify, autoconfirm, time_depart = handler_db.fetch_user_for_plan(
                     user_id)
                 fio = f'{user_id} {surname} {name} '  # TODO почему-то бывает периодчески по некоторым значениям возвращает None
-                counter_users += 1
                 if not password or not plan_notify:
                     continue
                 if '07:00' > current_time > '00:00' and not night_notify:  # обычно ['key'] выдает ошибку в некоторых местах нет ключа keyerror хотя ключ есть, а с методом get ключ видит
@@ -42,17 +52,45 @@ def cycle_plan_notify():
                         if notification.split()[0] == 'Проблема':
                             time.sleep(300)
                             continue
+
+                        # if messaging:
+                        #     new_document = check_news.parser(tab_number, password)
+                        #     availability_for_planing = True
+                        #     path_plan = "/usr/local/bin/bot/plans/plan" + str(user_id) + ".txt"
+                        #     with open(path_plan, 'r', encoding='utf-8') as original: # TODO выдает ошибку нет такойц директории, хотя файл есть
+                        #         old_file = original.read()
+                        #         if 'не найдено' in old_file or 'Отпуск' in old_file:
+                        #             availability_for_planing = False
+                        #
+                        #     if new_document is not None and availability_for_planing:
+                        #         doc_path = "/usr/local/bin/bot/documents/doc" + str(user_id) + ".txt"
+                        #
+                        #         if not os.path.exists(doc_path):
+                        #             with open(doc_path, 'w', encoding='utf-8') as new:
+                        #                 new.write(new_document)
+                        #                 bot.send_message(user_id, new_document, reply_markup=btn_doc, parse_mode='html')
+                        #
+                        #         else:
+                        #             with open(doc_path, 'r', encoding='utf-8') as old_file:
+                        #                 old_file = old_file.read()
+                        #                 if new_document not in old_file:
+                        #                     with open(doc_path, 'a', encoding='utf-8') as new_data:
+                        #                         new_data.write(f"\n\n{new_document}")
+                        #                         bot.send_message(user_id, new_document, reply_markup=btn_doc,
+                        #                                          parse_mode='html')
+                        #         bot.send_message(157758328, f'{fio} {new_document}', reply_markup=btn_doc,
+                        #                          parse_mode='html')
+                        #
+                        #     sent_doc_counter += 1
+                        #     sent_doc_list.append(fio)
+
                         if notification != None:  # не равно none - получили план. будет ошибка, если ему не удалось отправить ему его план - по
-                            plan_btn: InlineKeyboardMarkup = types.InlineKeyboardMarkup()  # что такое двоеточие и что оно дает???
-                            btn = types.InlineKeyboardButton(text="Открыть план работ в OpenSky",
-                                                             url='https://edu.rossiya-airlines.com/workplan/')
-                            plan_btn.add(btn)
                             bot.send_message(user_id, notification, reply_markup=plan_btn, parse_mode='html')
                             sent_plan_counter += 1
                             sent_plan_list.append(fio)
+
                     except Exception as exc:
-                        exc_event = exception_logger.writer(exc=exc,
-                                                            request="отправка плана пользователю в атоматическом режиме",
+                        exc_event = exception_logger.writer(exc=exc, request="автоотправка плана ",
                                                             fio=fio, answer='не удалось отправить план')
                         bot.send_message(157758328, f'не удалось отправить план: \n{exc_event}')
                         continue
@@ -60,9 +98,12 @@ def cycle_plan_notify():
             if sent_plan_counter > 0:
                 bot.send_message(157758328,
                                  f'общий отчет: план выслан {sent_plan_counter} чел. {", ".join(sent_plan_list)}')
+            if sent_doc_counter > 0:
+                bot.send_message(157758328,
+                                 f'общий отчет: документы высланы {sent_doc_counter} чел. {", ".join(sent_doc_list)}')
         except Exception as e:
             exception_logger.writer(exc=e, request="извлечение ключа словаря user_id при автоматической отправке плана",
                                     fio=fio, answer='поймали ошибку во внешнем try except')
             bot.send_message(157758328, f'поймали ошибку во внешнем try except: {fio}: {traceback.format_exc()}')
         # bot.send_message(157758328, f'Бот закончил проверку планов пользователей и лег спать на 5 мин.')
-        time.sleep(300)
+        time.sleep(200)
