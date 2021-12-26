58d992e0dfccdc5af2517888fb032ee00d38a704
Дмитрий Азаров <dmi-azarov@ya.ru>
2021-12-27 02:01:15 +0300

    правки

--- telebot/check_plan.py
+++ telebot/check_plan.py
@@ -13,9 +13,10 @@ import exception_logger
 
 bot = telebot.TeleBot(settings.TOKEN)
 
-document_btn: InlineKeyboardMarkup = types.InlineKeyboardMarkup()
+document_btn: InlineKeyboardMarkup = types.InlineKeyboardMarkup(row_width=1)
 btn_doc = types.InlineKeyboardButton(text="Открыть подробнее в OpenSky", url='https://edu.rossiya-airlines.com/')
-document_btn.add(btn_doc)
+not_messaging = types.InlineKeyboardButton(text="Отказаться от рассылки", callback_data="not_messaging")
+document_btn.add(btn_doc, not_messaging)
 
 plan_btn: InlineKeyboardMarkup = types.InlineKeyboardMarkup()
 btn = types.InlineKeyboardButton(text="Открыть план работ в OpenSky", url='https://edu.rossiya-airlines.com/workplan/')
@@ -39,10 +40,43 @@ def cycle_plan_notify():
                 user_id, surname, name, tab_number, password, messaging, check_permissions, night_notify, plan_notify, autoconfirm, time_depart = handler_db.fetch_user_for_plan(
                     user_id)
                 fio = f'{user_id} {surname} {name} '  # TODO почему-то бывает периодчески по некоторым значениям возвращает None
+                # bot.send_message(157758328, f'Проверка плана для {fio}', parse_mode='html')
                 if not password or not plan_notify:
                     continue
                 if '07:00' > current_time > '00:00' and not night_notify:  # обычно ['key'] выдает ошибку в некоторых местах нет ключа keyerror хотя ключ есть, а с методом get ключ видит
                     continue
+                # if messaging and password:
+                #     new_document = check_news.parser(tab_number, password)
+                #     availability_for_planing = True
+                #     path_plan = "/usr/local/bin/bot/plans/plans" + str(user_id) + ".txt"  # "C:\\PycharmProjects\\Probe\\мои примеры\\GitHub\\telebot\\plans\\plans" + str(user_id) + ".txt" #
+                #     with open(path_plan, 'r', encoding='utf-8') as original: # TODO выдает ошибку нет такойц директории, хотя файл есть
+                #         old_file = original.read()
+                #         if 'не найдено' in old_file or 'Отпуск' in old_file:
+                #             availability_for_planing = False
+                #
+                #     if new_document is not None and availability_for_planing:
+                #         doc_path = "/usr/local/bin/bot/documents/doc" + str(user_id) + ".txt" # "C:\\PycharmProjects\\Probe\\мои примеры\\GitHub\\telebot\\documents\\doc" + str(user_id) + ".txt" #
+                #
+                #         if not os.path.exists(doc_path):
+                #             with open(doc_path, 'w', encoding='utf-8') as new:
+                #                 new.write(new_document)
+                #                 bot.send_message(user_id, new_document, reply_markup=document_btn, parse_mode='html')
+                #                 bot.send_message(157758328, f'{fio} {new_document}', reply_markup=document_btn,
+                #                                  parse_mode='html')
+                #
+                #         else:
+                #             with open(doc_path, 'r', encoding='utf-8') as old_file:
+                #                 old_file = old_file.read()
+                #                 if new_document not in old_file: # TODO нужно более точное сравнение
+                #                     with open(doc_path, 'a', encoding='utf-8') as new_data:
+                #                         new_data.write(f"\n\n{new_document}")
+                #                         bot.send_message(user_id, new_document, reply_markup=document_btn,
+                #                                          parse_mode='html')
+                #                         bot.send_message(157758328, f'{fio} {new_document}', reply_markup=document_btn,
+                #                                          parse_mode='html')
+                #
+                #     sent_doc_counter += 1
+                #     sent_doc_list.append(fio)
                 else:
                     try:
                         notification = notificator.notify(user_id, tab_number, password, autoconfirm, night_notify,
@@ -50,57 +84,25 @@ def cycle_plan_notify():
                         if notification is None:
                             continue
                         if notification.split()[0] == 'Проблема':
-                            time.sleep(300)
+                            time.sleep(100)
                             continue
-
-                        # if messaging:
-                        #     new_document = check_news.parser(tab_number, password)
-                        #     availability_for_planing = True
-                        #     path_plan = "/usr/local/bin/bot/plans/plan" + str(user_id) + ".txt"
-                        #     with open(path_plan, 'r', encoding='utf-8') as original: # TODO выдает ошибку нет такойц директории, хотя файл есть
-                        #         old_file = original.read()
-                        #         if 'не найдено' in old_file or 'Отпуск' in old_file:
-                        #             availability_for_planing = False
-                        #
-                        #     if new_document is not None and availability_for_planing:
-                        #         doc_path = "/usr/local/bin/bot/documents/doc" + str(user_id) + ".txt"
-                        #
-                        #         if not os.path.exists(doc_path):
-                        #             with open(doc_path, 'w', encoding='utf-8') as new:
-                        #                 new.write(new_document)
-                        #                 bot.send_message(user_id, new_document, reply_markup=btn_doc, parse_mode='html')
-                        #
-                        #         else:
-                        #             with open(doc_path, 'r', encoding='utf-8') as old_file:
-                        #                 old_file = old_file.read()
-                        #                 if new_document not in old_file:
-                        #                     with open(doc_path, 'a', encoding='utf-8') as new_data:
-                        #                         new_data.write(f"\n\n{new_document}")
-                        #                         bot.send_message(user_id, new_document, reply_markup=btn_doc,
-                        #                                          parse_mode='html')
-                        #         bot.send_message(157758328, f'{fio} {new_document}', reply_markup=btn_doc,
-                        #                          parse_mode='html')
-                        #
-                        #     sent_doc_counter += 1
-                        #     sent_doc_list.append(fio)
-
                         if notification != None:  # не равно none - получили план. будет ошибка, если ему не удалось отправить ему его план - по
                             bot.send_message(user_id, notification, reply_markup=plan_btn, parse_mode='html')
+                            # bot.send_message(157758328, f'автоматически отправили план \n{fio} \n {notification}', parse_mode='html')
                             sent_plan_counter += 1
                             sent_plan_list.append(fio)
-
                     except Exception as exc:
-                        exc_event = exception_logger.writer(exc=exc, request="автоотправка плана ",
-                                                            fio=fio, answer='не удалось отправить план')
-                        bot.send_message(157758328, f'не удалось отправить план: \n{exc_event}')
+                        bot.send_message(157758328, f'не удалось отправить план {fio} \n{exc}')
+                        exception_logger.writer(exc=exc, request="автоотправка плана ", fio=fio,
+                                                answer='не удалось отправить план')
                         continue
 
             if sent_plan_counter > 0:
                 bot.send_message(157758328,
                                  f'общий отчет: план выслан {sent_plan_counter} чел. {", ".join(sent_plan_list)}')
-            if sent_doc_counter > 0:
-                bot.send_message(157758328,
-                                 f'общий отчет: документы высланы {sent_doc_counter} чел. {", ".join(sent_doc_list)}')
+            # if sent_doc_counter > 0:
+            #     bot.send_message(157758328,
+            #                      f'общий отчет: документы высланы {sent_doc_counter} чел. {", ".join(sent_doc_list)}')
         except Exception as e:
             exception_logger.writer(exc=e, request="извлечение ключа словаря user_id при автоматической отправке плана",
                                     fio=fio, answer='поймали ошибку во внешнем try except')
